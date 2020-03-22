58e5dc0bb1b7b9464b7e286eac223a40639c03ae
davidorjuela <davidorjuel@gmail.com>
2020-03-22 13:57:53 -0500

    9a.V0 app express

--- app.js
+++ app.js
@@ -8,20 +8,57 @@ mongoose.connection.on("error", function(e) { console.error(e); });
 
 // definimos el schema
 var schema = mongoose.Schema({
-    date: Date,
+    count: Number,
     name: String,
   });
  
-  var Visitor = mongoose.model("Visitor", schema);
-
+var Visitor = mongoose.model("Visitor", schema);
+var visitantes;
 app.get('/', (req, res) => {
-    var name='Anónimo'
-    req.query.name ? name=req.query.name : name=name;
-    var visitor = new Visitor({ name: name, date: new Date() });
-        visitor.save(function(err) {
-        if (err) return console.error(err);
+
+
+    if(req.query.name){
+        Visitor.findOne({'name':req.query.name}, function(err, visitorUpdate) {
+            if(visitorUpdate){
+                visitorUpdate.count += 1;
+                visitorUpdate.save(function(err) {
+                    if (err) return console.error(err);
+                });
+            }else{
+                var visitor = new Visitor({ name: req.query.name, count:1 });
+                visitor.save(function(err) {
+                    if (err) return console.error(err);
+                  });
+            }
+        });
+    }
+    else{
+        var visitor = new Visitor({ name: 'Anónimo', count:1 });
+        visitor.save();
+    }
+    
+    
+
+    Visitor.find({},function(err,visitors){
+        var html=`<table style="width:100%">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Visits</th>
+    </tr>`;
+    
+    visitors.forEach(visitor => {
+        html+=`
+        <tr>
+            <td>${visitor._id}</td>
+            <td>${visitor.name}</td>
+            <td>${visitor.count}</td>
+        </tr>`;
     });
 
-    res.send(`<h1>El visitante fue almacenado con éxito</h1>`);
+    html+=`</table>`;
+    res.send(html);
+    });
+    
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
